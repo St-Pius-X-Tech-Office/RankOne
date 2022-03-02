6da070e96ca48ae4fc41d749a49f1b330b9ef00d
Jon <jon.nguyen7@protonmail.com>
2022-03-02 09:00:49 -0600

    completed TODO's and added functions

--- rankone.py
+++ rankone.py
@@ -3,6 +3,7 @@ import pandas as pd
 import datetime
 import dateutil
 import json
+from datetime import timedelta
 
 
 # Load credentials from JSON File
@@ -29,9 +30,13 @@ print(response)
 headers["Authorization"] = "bearer " + response.json()["access_token"]
 headers["Content-Type"] = "application/json"
 
-# TODO: Add date formatting to schedURL
+# Get the dates for the sched_url
+today = datetime.datetime.today()
+lastWeek = today - timedelta(7)
+
+# Create URL variables for simpler readability when calling
 base_url = "https://api.rankonesport.com/PartnerAPI"
-sched_url = "/ScheduleUpdates/?ModifiedDateStart=20220201 00:00:00&ModifiedDateEnd=20220223 00:00:00"
+sched_url = f"/ScheduleUpdates/?ModifiedDateStart={lastWeek.strftime('%Y%m%d')} 00:00:00&ModifiedDateEnd={today.strftime('%Y%m%d')} 00:00:00"
 
 new = base_url + sched_url
 sched_response = requests.get(new, data=credentials["credentials"], headers=headers)
@@ -43,39 +48,75 @@ data = sched_response.json()
 # Add the content to a Pandas DataFrame
 out = pd.DataFrame(data)
 
-# TODO: Move the variables above base and sched URL
-# Filter for date only
-today = datetime.datetime.today()
+# Filter the column for dates that occured at least 1 month out
 monthFix = dateutil.relativedelta.relativedelta(months=1)
 newDate = today - monthFix
+# The 'OUT' will be used for both the scoring and scheduleChange functions
 out = out[pd.to_datetime(out["StartDate"]) >= newDate]
-out.to_csv("schedule-update.csv", index=False)
+out.to_csv("test.csv", index=False)
 
-# TODO: Add filtering and column selections to it's own function
-# Modify Dataframe for legibility
-out["StartDate"] = pd.to_datetime(out["StartDate"]).dt.strftime("%m-%d-%Y %I:%M%p")
-out["EndDate"] = pd.to_datetime(out["StartDate"]).dt.strftime("%m-%d-%Y %I:%M%p")
 
-# Filter out columns we need for schedule change requests
-schedule_Change = out[
-    [
-        "SportName",
-        "SportGender",
-        "LevelName",
-        "StartDate",
-        "EndDate",
-        "DateModified",
-        "FacilityName",
-        "Address",
-        "City",
-        "State",
-        "Zip",
-        "ScheduleDescription",
-        "GameStatus",
+def scheduleChanges():
+    # Modify Dataframe for legibility
+    out["StartDate"] = pd.to_datetime(out["StartDate"]).dt.strftime("%m-%d-%Y %I:%M%p")
+    out["EndDate"] = pd.to_datetime(out["StartDate"]).dt.strftime("%m-%d-%Y %I:%M%p")
+    out["DateModified"] = pd.to_datetime(out["DateModified"]).dt.strftime("%m-%d-%Y")
+
+    # Filter out columns we need for schedule change requests
+    schedule_Change = out[
+        [
+            "SportName",
+            "LevelName",
+            "StartDate",
+            "EndDate",
+            "DateModified",
+            "FacilityName",
+            "Address",
+            "City",
+            "State",
+            "Zip",
+            "ScheduleDescription",
+            "GameStatus",
+            "CombinedScore",
+            "GameType",
+        ]
     ]
-]
 
-# Output the file without index
-schedule_Change.to_csv("newtest.csv", index=False)
+    # Output the file without index
+    schedule_Change.to_csv("ScheduleChanges.csv", index=False)
 
-# TODO: add a function for filtering based on added game scores
+
+def scoreUpdates():
+    """This function will be used to filter out the rows that include a score onlye. Any
+    blank rows will be left out."""
+
+    # Call a new dataframe called scores based off the "out" dataframe
+    scores = out
+    # Filter the scores df to remove blank rows
+    scores = scores[scores["CombinedScore"].str.len() > 0]
+
+    # Filter the columns needed
+    scoreUpdate = scores[
+        [
+            "SportName",
+            "LevelName",
+            "StartDate",
+            "EndDate",
+            "FacilityName",
+            "Address",
+            "City",
+            "State",
+            "Zip",
+            "ScheduleDescription",
+            "GameStatus",
+            "CombinedScore",
+            "GameType",
+        ]
+    ]
+
+    scoreUpdate.to_csv("ScoreChanges.csv", index=False)
+
+
+# Call the functions as needed
+scheduleChanges()
+scoreUpdates()
